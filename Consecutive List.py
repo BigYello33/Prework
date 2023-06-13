684155e5cc3626d5626078754520d5ab3a6fd0fe
unknown <darnellwarren64@gmail.com>
2023-06-13 14:45:58 -0400

    first commit

---
+++ Consecutive List.py
@@ -0,0 +1,12 @@
+
+def is_consecutive(a_list):
+    return (a_list)
+a_list = [1,2,3,4]
+for numbers in a_list:
+        if a_list == list(range(1,6)):
+            print("Consecutive")
+        else:
+            print("Not Consecutive")
+
+print(is_consecutive(a_list))
+
