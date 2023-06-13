5c27e49c74468fa98fde0d5311fcbce24380c6af
unknown <darnellwarren64@gmail.com>
2023-06-13 14:48:42 -0400

    updating questions 1-4

--- Consecutive List.py
+++ Consecutive List.py
@@ -1,3 +1,44 @@
+def hello_name():
+    message=input("What is your name?: ")
+
+    print(message)
+print(hello_name())
+
+def first_odds():
+    x=1
+    while True:
+        print(x)
+        x+=2
+        if x>100:
+            break
+print(first_odds())
+
+def max_num_in_list(a_list):
+    return x
+number= list(range(1,7))
+for x in number:
+    print(max(number))
+    break
+
+def is_leap_year(a_year):
+    return a_year
+
+x = int(input("Is this year a Leap Year?: "))
+
+if ((x%4==0) and (x%100!=0)) is True: 
+    print("\nThe year is a leap year!")
+    
+elif (x%400==0) is True:
+    print("\nThe year is a leap year!")
+else:
+    print("\nThis is NOT a leap year") 
+    
+print(is_leap_year(x))
+
+
+
+
+
 
 def is_consecutive(a_list):
     return (a_list)
